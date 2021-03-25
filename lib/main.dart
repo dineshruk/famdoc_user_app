30031f7cebf7562990706c2e48980c190b4e4a90
dineshruk <dineshrukshan225@gmail.com>
2021-03-25 17:24:51 +0530

    Changed Title

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demoa Home Page'),
+      home: MyHomePage(title: 'Flutter Famdoc App'),
     );
   }
 }
